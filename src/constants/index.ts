c5552af099d0b879ee88c31c8abe8b0614cee972
mul53 <mulengabowa53@gmail.com>
2021-08-02 11:22:18 +0200

    remove multicall

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,5 +7,3 @@ export enum NetworkId {
 export const UNISWAP_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2'
 export const FUSESWAP_SUBGRAPH_URL = 'https://graph.fuse.io/subgraphs/name/fuseio/fuseswap'
 export const PANCAKESWAP_SUBGRAPH_URL = 'https://bsc.streamingfast.io/subgraphs/name/pancakeswap/exchange-v2'
-
-export const MULTICALL_ADDRESS = '0x3CE6158b7278Bf6792e014FA7B4f3c6c46fe9410'
