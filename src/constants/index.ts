6ff2e9a799a196fc3b16e3bf52d5a2c69e6c7636
mul53 <mulengabowa53@gmail.com>
2022-03-09 20:23:52 +0200

    add production values

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,8 +8,11 @@ export const UNISWAP_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/uni
 export const FUSESWAP_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/fuseio/fuseswap'
 export const PANCAKESWAP_SUBGRAPH_URL = 'https://bsc.streamingfast.io/subgraphs/name/pancakeswap/exchange-v2'
 
-export const MASTERCHEF_V2_ADDRESS = '0xDb12c424105ecdE61d37161034f92EAcF44caf82'
-export const MASTERCHEF_V3_ADDRESS = '0x7c416513BEf5D21950af85742d9644F387Dd08F4'
+export const MASTERCHEF_V2_ADDRESS = '0xc71E27C7e128d9CAEb2b8cA756647f7F199cF39e'
+export const MASTERCHEF_V3_ADDRESS = '0xE3e184a7b75D0Ae6E17B58F5283b91B4E0A2604F'
 
-export const MASTERCHEF_V2_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/mul53/masterchefv2'
-export const MASTERCHEF_V3_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/mul53/masterchefv3'
+export const MASTERCHEF_V2_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/voltfinance/masterchefv2'
+export const MASTERCHEF_V3_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/voltfinance/masterchef-v3'
+
+export const xVOLT = '0x97a6e78c9208c21afaDa67e7E61d7ad27688eFd1'
+export const VOLT = '0x34Ef2Cc892a88415e9f02b91BfA9c91fC0bE6bD4'
