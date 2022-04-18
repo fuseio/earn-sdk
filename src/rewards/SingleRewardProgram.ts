1ea5dabffdde31896abec0c8c541d866af505588
t0mcr8se <husamirami@gmail.com>
2022-04-19 02:22:26 +0300

    refactor single rewards

--- src/rewards/SingleRewardProgram.ts
+++ src/rewards/SingleRewardProgram.ts
@@ -233,13 +233,11 @@ export default class SingleRewardProgram extends RewardProgram {
           apyPercent
         }
       ],
-      token0,
-      token1,
+      tokens: [token0, token1],
       totalStakedUSD,
       globalTotalStakeUSD,
       pairPrice,
-      reserve0: reserve0.toFixed(),
-      reserve1: reserve1.toFixed(),
+      reserves: [reserve0.toFixed(), reserve1.toFixed()],
       lockedRewards: lockedRewards.toFixed()
     }
   }
