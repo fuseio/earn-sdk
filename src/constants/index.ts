7d0a93a6d109b2c0bf874bfd6daf5fb11d63b631
mul53 <mulengabowa53@gmail.com>
2024-06-12 09:35:55 +0200

    upgrade subgraphs (#40)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,16 +5,16 @@ export enum NetworkId {
 }
 
 export const UNISWAP_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2'
-export const FUSESWAP_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/fuseio/fuseswap'
-export const VOLTAGE_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/voltfinance/voltage-exchange-v2'
+export const FUSESWAP_SUBGRAPH_URL = 'https://gateway-arbitrum.network.thegraph.com/api/126e6c43e26f28a5e5a8c8e216edbb89/subgraphs/id/8M4V5DnbjSWzYjDt9XUTZsiqX2cXWVcTd9nPw68NJdN4'
+export const VOLTAGE_SUBGRAPH_URL = 'https://gateway-arbitrum.network.thegraph.com/api/126e6c43e26f28a5e5a8c8e216edbb89/subgraphs/id/B4BGk9itvmRXzzNRAzBWwQARHRt3ZvLz11aWNVsZPT4'
 export const PANCAKESWAP_SUBGRAPH_URL = 'https://bsc.streamingfast.io/subgraphs/name/pancakeswap/exchange-v2'
-export const STABLESWAP_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/voltfinance/stableswap'
+export const STABLESWAP_SUBGRAPH_URL = 'https://gateway-arbitrum.network.thegraph.com/api/126e6c43e26f28a5e5a8c8e216edbb89/subgraphs/id/HTFuhfjdwFoc3wXGQ5UMrevui5mjx2N6pJnratYrCzSa'
 
 export const MASTERCHEF_V2_ADDRESS = '0xc71E27C7e128d9CAEb2b8cA756647f7F199cF39e'
 export const MASTERCHEF_V3_ADDRESS = '0xE3e184a7b75D0Ae6E17B58F5283b91B4E0A2604F'
 
 export const MASTERCHEF_V2_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/voltfinance/masterchefv2'
-export const MASTERCHEF_V3_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/voltfinance/masterchef-v3'
+export const MASTERCHEF_V3_SUBGRAPH_URL = 'https://gateway-arbitrum.network.thegraph.com/api/126e6c43e26f28a5e5a8c8e216edbb89/subgraphs/id/4DwVLaAaEuutpoCwmGUNBS45mSnGABt42u1Qbf73BqbR'
 
 export const xVOLT = '0x97a6e78c9208c21afaDa67e7E61d7ad27688eFd1'
 export const VOLT = '0x34Ef2Cc892a88415e9f02b91BfA9c91fC0bE6bD4'
